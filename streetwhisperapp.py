c27cd7576485c4f2ee2acdc89ce8d202370fc088
Emily Su <emilyzqsu@gmail.com>
2024-04-06 03:01:52 -0400

    Updated CLI so that it checks access token

--- streetwhisperapp.py
+++ streetwhisperapp.py
@@ -2,31 +2,68 @@ from backend import whisper_with_diarization_as_methods
 import typer
 from PyInquirer import prompt
 from rich import print as rprint
-import subprocess
+import whisperx
 
 app = typer.Typer()
 
 @app.command("start")
-def questions_ui():
+def startup_ui():
     rprint("[magenta]=============================[magenta]")
     rprint("[bold][underline]STREET Lab Whisper App[underline][bold]")
     rprint("[magenta]=============================[magenta]")
-    # Process selection
-    translation_transcription_prompt = [
+    authorization()
+
+def authorization():
+    rprint(f"What is your access token? An access token from Hugging Face and accepting pyannote/speaker-diarization-3.1's user condition is needed to run the app. If this is not your first time running the app, you can just press enter on your keyboard.[bold]")
+    potential_access_token = input()
+    try:
+        # Check token
+        diarize_model = whisperx.DiarizationPipeline(device="cpu", use_auth_token=potential_access_token)
+        questions_ui(diarize_model)
+        typer.Exit()
+    except:
+        invalidTokenPrompt = [
             {
                 'type': 'list',
-                'name': 'process_selected',
-                'message': 'What process do you want to run on your audio file?',
+                'name': 'invalid_token',
+                'message': 'This token is invalid. Do you want to try again?',
                 'choices': [
-                            {
-                                'name': 'Transcription Only',
-                            },
-                            {
-                                'name': 'Translation Only',
-                            }
+                    {
+                        'name': 'Yes',
+                    },
+                    {
+                        'name': 'No',
+                    }
                 ],
             }
         ]
+        invalid_token = prompt(invalidTokenPrompt)
+        if invalid_token["invalid_token"] == 'Yes':
+            authorization()
+        else:
+            typer.Exit()
+
+
+def questions_ui(diarize_model):
+    rprint("[magenta]=============================[magenta]")
+    rprint(f"You can now go offline.[bold]")
+    rprint("[magenta]=============================[magenta]")
+    # Process selection
+    translation_transcription_prompt = [
+        {
+            'type': 'list',
+            'name': 'process_selected',
+            'message': 'What process do you want to run on your audio file?',
+            'choices': [
+                {
+                    'name': 'Transcription Only',
+                },
+                {
+                    'name': 'Translation Only',
+                }
+            ],
+        }
+    ]
     process_selected = prompt(translation_transcription_prompt)
     # Input file
     rprint("[blue]=============================[blue]")
@@ -36,42 +73,42 @@ def questions_ui():
     rprint("[blue]=============================[blue]")
     # Is Input File in English?
     to_eng_selection_prompt = [
-            {
-                'type': 'list',
-                'name': 'to_english_selection',
-                'message': 'Is your audio file in English?',
-                'choices': [
-                            {
-                                'name': 'Yes',
-                            },
-                            {
-                                'name': 'No',
-                            },
-                ],
-            }
-        ]
+        {
+            'type': 'list',
+            'name': 'to_english_selection',
+            'message': 'Is your audio file in English?',
+            'choices': [
+                {
+                    'name': 'Yes',
+                },
+                {
+                    'name': 'No',
+                },
+            ],
+        }
+    ]
     to_english_selection = prompt(to_eng_selection_prompt)
 
     rprint("[blue]=============================[blue]")
     # Model size selection
     model_size_selection_prompt = [
-            {
-                'type': 'list',
-                'name': 'model_size_selection',
-                'message': 'What model size do you want to use on your audio file?',
-                'choices': [
-                            {
-                                'name': 'large-v2',
-                            },
-                            {
-                                'name': 'small',
-                            },
-                            {
-                                'name': 'medium',
-                            },
-                ],
-            }
-        ]
+        {
+            'type': 'list',
+            'name': 'model_size_selection',
+            'message': 'What model size do you want to use on your audio file?',
+            'choices': [
+                {
+                    'name': 'large-v2',
+                },
+                {
+                    'name': 'small',
+                },
+                {
+                    'name': 'medium',
+                },
+            ],
+        }
+    ]
     model_size_selection = prompt(model_size_selection_prompt)
     rprint("[blue]=============================[blue]")
     # Destination Folder
@@ -96,12 +133,11 @@ def questions_ui():
         ]
     questions_finished = prompt(questions_finished_prompt)
     if questions_finished["questions_finished"] == 'Yes':
-        run_process(process_selected["process_selected"], input_file, to_english_selection["to_english_selection"], model_size_selection["model_size_selection"], destination_selection)
+        # Run process
+        whisper_with_diarization_as_methods.main(process_selected["process_selected"], input_file, to_english_selection["to_english_selection"], model_size_selection["model_size_selection"], destination_selection, diarize_model)
     else:
+        # Exit out of app
         typer.Exit()
-
-def run_process(process_selected, input_file, to_english_selection, model_size_selection, destination_selection):
-    whisper_with_diarization_as_methods.main(str(process_selected), str(input_file), bool(to_english_selection), str(model_size_selection), str(destination_selection))
 
 @app.command("help")
 def help_ui():
