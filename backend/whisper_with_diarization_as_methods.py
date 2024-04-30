739270650e6106e4c722a07ec50c73b8c570e73c
Emily Su <emilyzqsu@gmail.com>
2024-04-30 17:08:49 -0400

    Reverted

--- backend/whisper_with_diarization_as_methods.py
+++ backend/whisper_with_diarization_as_methods.py
@@ -314,7 +314,7 @@ def main(process_selected: str, input_file: str, to_english_selection: bool, mod
         output_format = "translation"
     else: # reaching here means: process_selected == 'Transcription + Translation Only'
         output_csv_headers = ["Timestamps", "Speaker No", "Text[Orig Lang]", "Text[Eng]"]
-        output_format = "transcribe_translate"
+        output_format = "transcribe&translate"
 
     now = datetime.now()
     audio_path_last_backslash_index = input_file.rfind("/")
